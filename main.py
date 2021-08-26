8b013beee431305f2b329703d9d8da6b97562bdf
Juli Kuczkowska <julitakuczkowska@gmail.com>
2021-08-26 13:17:38 +0200

    first version

---
+++ main.py
@@ -0,0 +1,7 @@
+number1 = float(input("podaj liczbę 1: "))
+number2 = float(input("podaj liczbę 2: "))
+number3 = float(input("podaj liczbę 3: "))
+number4 = float(input("podaj liczbę 4: "))
+number5 = float(input("podaj liczbę 5: "))
+number_count = 5
+print((number1 + number2 + number3 + number4 + number5) / number_count)
